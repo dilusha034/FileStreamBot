3b59175a768d5e62e645f17a47194e9621a9e022
dilusha034 <ddilusha034@gmail.com>
2025-10-22 19:58:07 +0530

    Update render_template.py

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -1,36 +1,40 @@
-import os
-from aiohttp.web import Request
+import aiohttp
+import jinja2
+from aiohttp.web import Request  # <-- අලුතින් import කළ යුතුයි
 from FileStream.config import Telegram
-from FileStream.utils.file_properties import get_file_ids
+from FileStream.utils.database import Database
 from FileStream.utils.human_readable import humanbytes
 
-async def render_page(db_id: str, request: Request) -> str:
-    """
-    HTML ප්ලේයර් පිටුව සඳහා අවශ්‍ය දත්ත සකසා,
-    URL එක HTTPS වලින් ජනනය කර HTML එක ආපසු ලබා දීම
-    """
-    file_id = await get_file_ids(db_id, Telegram.MULTI_CLIENT)
-    
-    file_name = file_id.file_name
-    file_size = humanbytes(file_id.file_size)
+db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
-    # --- මෙන්න ප්‍රධානම නිවැරදි කිරීම ---
-    # request එකෙන් scheme (http/https) සහ host (domain name) එක ලබාගෙන
-    # නිවැරදි සහ සම්පූර්ණ URL එක ජනනය කිරීම.
+# function එක දැන් request එකත් බලාපොරොත්තු වෙනවා
+async def render_page(db_id: str, request: Request):
+    file_data = await db.get_file(db_id)
+    
+    # --- මෙතන තමයි ප්‍රධානම වෙනස ---
+    # Server.URL භාවිතා කරනවා වෙනුවට, request එකෙන් URL එක නිවැරදිව සකසා ගැනීම
     scheme = request.headers.get("X-Forwarded-Proto", request.scheme)
     host = request.host
-    file_url = f"{scheme}://{host}/dl/{db_id}"
+    src = f"{scheme}://{host}/dl/{file_data['_id']}"
+    # ------------------------------------
 
-    # play.html ගොනුව විවෘත කර දත්ත ආදේශ කිරීම
-    # __file__ මගින් මෙම ගොනුව ඇති ස්ථානය නිවැරදිවම සොයාගනී
-    template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "template", "play.html")
-    
-    with open(template_path, "r", encoding="utf-8") as template_file:
-        template_content = template_file.read()
+    file_size = humanbytes(file_data['file_size'])
+    file_name = file_data['file_name'].replace("_", " ")
 
-    # {{...}} placeholders වලට අදාළ අගයන් ආදේශ කිරීම
-    rendered_html = template_content.replace("{{file_name}}", file_name)
-    rendered_html = rendered_html.replace("{{file_size}}", file_size)
-    rendered_html = rendered_html.replace("{{file_url}}", file_url)
-    
-    return rendered_html
+    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
+        template_file = "FileStream/template/play.html"
+    else:
+        template_file = "FileStream/template/dl.html"
+        # ඔබගේ මුල් කේතයේ තිබූ aiohttp කොටස වෙනස්කම් නොමැතිවම ක්‍රියා කරයි
+        async with aiohttp.ClientSession() as s:
+            async with s.get(src) as u:
+                file_size = humanbytes(int(u.headers.get('Content-Length')))
+
+    with open(template_file, "r", encoding="utf-8") as f: # utf-8 encoding එකතු කිරීම වඩාත් සුදුසුයි
+        template = jinja2.Template(f.read())
+
+    return template.render(
+        file_name=file_name,
+        file_url=src,
+        file_size=file_size
+    )
