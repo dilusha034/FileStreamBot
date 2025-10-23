426dfdec959303d56cc455f00d4e2a2fd6133ecb
dilusha034 <ddilusha034@gmail.com>
2025-10-23 18:31:31 +0530

    Update __init__.py

--- FileStream/server/__init__.py
+++ FileStream/server/__init__.py
@@ -1,7 +1,8 @@
 from aiohttp import web
 from .stream_routes import routes
 
-def web_server():
-    web_app = web.Application(client_max_size=30000000)
+# This is the web_server function that your __main__.py is looking for.
+async def web_server():
+    web_app = web.Application()
     web_app.add_routes(routes)
     return web_app
