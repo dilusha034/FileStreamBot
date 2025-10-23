d8a2613d2ff27728d012e44d3127b5625af30397
dilusha034 <ddilusha034@gmail.com>
2025-10-23 18:37:24 +0530

    Update __init__.py

--- FileStream/__init__.py
+++ FileStream/__init__.py
@@ -1,7 +1,4 @@
-from aiohttp import web
-from .stream_routes import routes
+import time
 
-async def web_server():
-    web_app = web.Application()
-    web_app.add_routes(routes)
-    return web_app
+version = "1.1.0"
+StartTime = time.time()
