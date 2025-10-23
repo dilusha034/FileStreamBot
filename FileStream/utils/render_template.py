e7892f12b12670dfbc3d68f9343659be24dd6a47
dilusha034 <ddilusha034@gmail.com>
2025-10-23 17:35:50 +0530

    Update render_template.py

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -1,36 +1,30 @@
 import aiohttp
-from aiohttp.web import Request
-from FileStream.config import Telegram
+import jinja2
+import urllib.parse
+from FileStream.config import Telegram, Server
 from FileStream.utils.database import Database
 from FileStream.utils.human_readable import humanbytes
-from FileStream.server.exceptions import FIleNotFound
-import aiofiles
-
 db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
-async def render_page(db_id: str, request: Request):
-    file_data = await db.get_file(db_id)
+async def render_page(db_id):
+    file_data=await db.get_file(db_id)
+    src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
+    file_size = humanbytes(file_data['file_size'])
+    file_name = file_data['file_name'].replace("_", " ")
 
-    if not file_data:
-        raise FIleNotFound
+    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
+        template_file = "FileStream/template/play.html"
+    else:
+        template_file = "FileStream/template/dl.html"
+        async with aiohttp.ClientSession() as s:
+            async with s.get(src) as u:
+                file_size = humanbytes(int(u.headers.get('Content-Length')))
 
-    file_name = file_data.get('file_name', 'Unknown File').replace("_", " ")
-    file_size = humanbytes(file_data.get('file_size', 0))
+    with open(template_file) as f:
+        template = jinja2.Template(f.read())
 
-    # Construct the download URL correctly
-    scheme = request.headers.get("X-Forwarded-Proto", request.scheme)
-    host = request.host
-    dl_url = f"{scheme}://{host}/dl/{db_id}"
-
-    try:
-        async with aiofiles.open("FileStream/template/play.html", mode='r', encoding='utf-8') as f:
-            html = await f.read()
-    except FileNotFoundError:
-        return "Template file not found"
-
-    # Replace placeholders
-    html = html.replace("{{file_name}}", file_name)
-    html = html.replace("{{file_size}}", file_size)
-    html = html.replace("{{dl_url}}", dl_url)
-
-    return html
+    return template.render(
+        file_name=file_name,
+        file_url=src,
+        file_size=file_size
+    )
